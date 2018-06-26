7e220a737d2f16cd6cbda9827bdb818bb3bd0580
vinayd1 <agrawalv4@gmail.com>
2018-06-26 23:15:56 +0530

    faker

--- Server/routes/seller.js
+++ Server/routes/seller.js
@@ -7,6 +7,8 @@ const multer = require('multer');
 const multerS3 = require('multer-s3');
 const s3 = new aws.S3(config.s3);
 
+const faker = require('faker');
+
 const checkJWT = require('../middlewares/check-jwt');
 
 const upload = multer({
@@ -24,7 +26,31 @@ const upload = multer({
 });
 
 router.route('/products')
-    .get()
+    .get(checkJWT, (req, res, next) => {
+        Product.find({owner: req.decoded.user._id})
+            .populate('owner')
+            .populate('category')
+            .exec((err, products) => {
+                if(err) {
+                    res.json({
+                        success: false,
+                        message: err.message
+                    });
+                }
+                if(products) {
+                    res.json({
+                        success: true,
+                        message: "Products",
+                        products: products
+                    });
+                }  else {
+                    res.json({
+                        success: false,
+                        message: "No product found"
+                    });
+                }
+            });
+    })
     .post([checkJWT, upload.single('productPicture')], (req, res, next) => {
         let product = new Product();
         console.log("3");
@@ -48,4 +74,32 @@ router.route('/products')
         })
     });
 
+
+/******************Just For Testing****************/
+
+router.get('/faker/test', (req, res, next) => {
+    for (i = 0; i < 20; i++) {
+        let product = new Product();
+        product.category = "5b26a066a10b1c54ba724e13";
+        product.owner = "5b269506166b474bae261250";
+        product.image = faker.image.cats();
+        product.title = faker.commerce.productName();
+        product.description = faker.lorem.words();
+        product.price = faker.commerce.price();
+        let save = product.save();
+        save.catch((error) => {
+            res.json({
+                success: false,
+                message: error.message
+            });
+        });
+    }
+
+    res.json({
+        success: true,
+        message: "Successfully added 20 products"
+    });
+});
+
+/*************************************************/
 module.exports = router;
