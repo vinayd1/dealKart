90f6fc81dcfad0629e418f4582cc7f815715cf91
vinayd1 <agrawalv4@gmail.com>
2018-08-24 00:08:45 +0530

    product-api

--- Server/routes/main.js
+++ Server/routes/main.js
@@ -1,5 +1,42 @@
 const router = require('express').Router();
+const async = require('async');
 const Category = require('../models/category');
+const Product = require('../models/product');
+
+router.get('/products', (req, res, next) => {
+    const perPage = 10;
+    const page = req.query.page;
+
+    async.parallel([
+        function(callback) {
+            Product.count({}, (err, count) => {
+                var totalProducts = count;
+                callback(err, totalProducts);
+            });
+        },
+        function(callback) {
+            Product.find({})
+            .skip(perPage*page)
+            .limit(perPage)
+            .populate('category')
+            .populate('owner')
+            .exec((err, products) => {
+                if(err) return next(err);
+                callback(err, products);
+            });
+        }
+    ], function(err, results) {
+        var totalProducts = results[0];
+        var products = results[1];
+        res.json({
+            success: true,
+            message: 'category',
+            products: products,
+            totalProducts: totalProducts,
+            pages: Math.ceil(totalProducts/perPage)
+        });
+    });
+});
 
 router.route('/categories')
     .get((req, res, next) => {
@@ -28,4 +65,84 @@ router.route('/categories')
             });
         });
     });
+
+router.get('/categories/:id', (req, res, next) => {
+    const perPage = 10;
+    
+    
+    // Product.find({ category: req.params.id })
+    //     .populate('category')
+    //     .exec((err, products) => {
+    //         Product.count({ category: req.params.id }, (err, totalProducts) => {
+    //             res.json({
+    //                 success: true,
+    //                 message: "category",
+    //                 products: products,
+    //                 categoryName: products[0].category.name,
+    //                 totalProducts: totalProducts,
+    //                 pages: Math.ceil(totalProducts/perPage)
+    //             });
+    //         });
+    //     });
+
+    const page = req.query.page;
+    async.parallel([
+        function(callback) {
+            Product.count({ category: req.params.id }, (err, count) => {
+                var totalProducts = count;
+                callback(err, totalProducts);
+            });
+        },
+        function(callback) {
+            Product.find({ category: req.params.id })
+            .skip(perPage*page)
+            .limit(perPage)
+            .populate('category')
+            .populate('owner')
+            .exec((err, products) => {
+                if(err) return next(err);
+                callback(err, products);
+            });
+        },
+        function(callback) {
+            Category.findOne({ _id: req.params.id}, (err, category) => {
+            callback(err, category);
+            });
+        }
+    ], function(err, results) {
+        var totalProducts = results[0];
+        var products = results[1];
+        var category = results[2];
+        res.json({
+            success: true,
+            message: 'category',
+            products: products,
+            categoryName: category.name,
+            totalProducts: totalProducts,
+            pages: Math.ceil(totalProducts/perPage)
+        });
+    });
+});
+
+router.get('/product/:id', (req, res, next) => {
+    Product.findById({ _id: req.params.id})
+        .populate('category')
+        .populate('owner')
+        .exec((err, product) => {
+            if(err) {
+                res.json({
+                    success: false,
+                    message: 'Product is not found'
+                });
+            } else {
+                if(product) {
+                    res.json({
+                        success: true,
+                        product: product
+                    });
+                }
+            }
+        });
+});
+
 module.exports = router;
