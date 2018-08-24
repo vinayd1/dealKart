e3dfd98dd741e8a131d1fa5d175e3b18be479341
vinayd1 <agrawalv4@gmail.com>
2018-08-24 23:01:35 +0530

    reviews

--- Client/AngularAmazono/src/app/product/product.component.ts
+++ Client/AngularAmazono/src/app/product/product.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RestApiService } from '../rest-api.service';
 import { DataService } from '../data.service';
-import { Route } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-product',
@@ -13,6 +12,14 @@ import { Route } from '@angular/compiler/src/core';
 export class ProductComponent implements OnInit {
 
   product: any;
+
+  myReview = {
+    title: '',
+    description: '',
+    rating: 0
+  };
+
+  btnDisabled = false;
 
   constructor(
     private data: DataService,
@@ -32,4 +39,24 @@ export class ProductComponent implements OnInit {
     })
   }
 
+  async postReview() {
+    this.btnDisabled = true;
+    try {
+      const data = await this.rest.post(
+        'http://localhost:3030/api/review',
+        {
+          productId: this.product._id,
+          title: this.myReview.title,
+          description: this.myReview.description,
+          rating: this.myReview.rating
+        }
+      );
+      data['success']
+        ? this.data.success(data['message'])
+        : this.data.error(data['message']);
+    } catch(error) {
+      this.data.error(error['message']);
+    }
+    this.btnDisabled = false;
+  } 
 }
