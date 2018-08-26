57a05684d716fdec5d834144d279c15fd2b1fe2c
vinayd1 <agrawalv4@gmail.com>
2018-08-27 01:07:57 +0530

    search-frontend

--- Client/AngularAmazono/src/app/search/search.component.ts
+++ Client/AngularAmazono/src/app/search/search.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RestApiService } from '../rest-api.service';
 import { DataService } from '../data.service';
+import { LowerCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-search',
@@ -23,6 +24,38 @@ export class SearchComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(res => {
+      this.query = res['query'];
+      this.page = 1;
+      this.getProducts();
+    });
+
+  }
+
+  get lower() {
+    return 1 + this.content.hitsPerPage * this.content.page;
+  }
+
+  get upper() {
+    return Math.min(
+      this.content.hitsPerPage * (this.content.page + 1),
+      this.content.nbHits
+    )
+  }
+
+  async getProducts() {
+    this.content = null;
+    try{
+      const data = await this.rest.get(
+        `http://localhost:3030/api/search?query=${this.query}&${this.page -1}`
+      );
+      data['success']
+        ? (this.content = data['content'])
+        : this.data.error(data['message']);
+        console.log(this.content);
+    } catch (error) {
+      this.data.error(error['message']);
+    }
   }
 
 }
